170234dc3ed50f4934fe9a0c751d5770b3411409
Parthu <parthudhanireddy@gmail.com>
2026-06-26 22:48:46 +0530

    Fix performance issues by adding DB indices for background jobs

--- src/test/java/com/fooddelivery/payments/service/WebhookDlqJobTest.java
+++ src/test/java/com/fooddelivery/payments/service/WebhookDlqJobTest.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
